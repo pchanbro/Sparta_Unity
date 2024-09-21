132c1f96847fb1abf75ba46180b98b65333def22
pchanbro <cksgud0509@naver.com>
2024-09-21 18:42:38 +0900

    C#공부
    학습하면서 실습한 내용, 거의 없다.

--- 2ed week/1.C#소개, 환경설정, 변수, 자료형, 연산자, 문자열 처리, 계산기/Chapter3/Chapter3/Program.cs
+++ 2ed week/1.C#소개, 환경설정, 변수, 자료형, 연산자, 문자열 처리, 계산기/Chapter3/Chapter3/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello, {0}!", name);
+
+            Console.Write("Enter two numbers: ");
+            string input = Console.ReadLine();    // "10 20"과 같은 문자열을 입력받음
+
+            string[] numbers = input.Split(' ');  // 문자열을 공백으로 구분하여 배열로 만듦
+            // { "10", "20" }
+            int num1 = int.Parse(numbers[0]);     // 첫 번째 값을 정수로 변환하여 저장, 형변환이랑은 다르다
+            int num2 = int.Parse(numbers[1]);     // 두 번째 값을 정수로 변환하여 저장
+
+            int sum = num1 + num2;                // 두 수를 더하여 결과를 계산
+
+            Console.WriteLine("The sum of {0} and {1} is {2}.", num1, num2, sum);
         }
     }
 }
